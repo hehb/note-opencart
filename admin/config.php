f5e59f2e897746b02f7ca5ea2824a59a488a6dfa
hbhe <hehbhehb@sina.com>
2016-02-01 16:21:36 +0800

    init
    Signed-off-by: hbhe <hehbhehb@sina.com>

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/note-opencart/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/note-opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/note-opencart/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/note-opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/htdocs/note-opencart/admin/');
+define('DIR_SYSTEM', 'D:/htdocs/note-opencart/system/');
+define('DIR_LANGUAGE', 'D:/htdocs/note-opencart/admin/language/');
+define('DIR_TEMPLATE', 'D:/htdocs/note-opencart/admin/view/template/');
+define('DIR_CONFIG', 'D:/htdocs/note-opencart/system/config/');
+define('DIR_IMAGE', 'D:/htdocs/note-opencart/image/');
+define('DIR_CACHE', 'D:/htdocs/note-opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/htdocs/note-opencart/system/storage/download/');
+define('DIR_LOGS', 'D:/htdocs/note-opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/htdocs/note-opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/htdocs/note-opencart/system/storage/upload/');
+define('DIR_CATALOG', 'D:/htdocs/note-opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'note-opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/note-opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/note-opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/htdocs/note-opencart/catalog/');
+define('DIR_SYSTEM', 'D:/htdocs/note-opencart/system/');
+define('DIR_LANGUAGE', 'D:/htdocs/note-opencart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/htdocs/note-opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/htdocs/note-opencart/system/config/');
+define('DIR_IMAGE', 'D:/htdocs/note-opencart/image/');
+define('DIR_CACHE', 'D:/htdocs/note-opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/htdocs/note-opencart/system/storage/download/');
+define('DIR_LOGS', 'D:/htdocs/note-opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/htdocs/note-opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/htdocs/note-opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'note-opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
