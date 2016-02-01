5d0c957d9189aa02347ddbca7033850c3f37eeb6
hbhe <hbhe@wosotech.com>
2016-02-01 16:37:03 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://127.0.0.1/note-opencart/admin/');
 define('HTTPS_CATALOG', 'http://127.0.0.1/note-opencart/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/htdocs/note-opencart/admin/');
-define('DIR_SYSTEM', 'D:/htdocs/note-opencart/system/');
-define('DIR_LANGUAGE', 'D:/htdocs/note-opencart/admin/language/');
-define('DIR_TEMPLATE', 'D:/htdocs/note-opencart/admin/view/template/');
-define('DIR_CONFIG', 'D:/htdocs/note-opencart/system/config/');
-define('DIR_IMAGE', 'D:/htdocs/note-opencart/image/');
-define('DIR_CACHE', 'D:/htdocs/note-opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/htdocs/note-opencart/system/storage/download/');
-define('DIR_LOGS', 'D:/htdocs/note-opencart/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/htdocs/note-opencart/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/htdocs/note-opencart/system/storage/upload/');
-define('DIR_CATALOG', 'D:/htdocs/note-opencart/catalog/');
+define('DIR_APPLICATION', 'C:/htdocs/note-opencart/admin/');
+define('DIR_SYSTEM', 'C:/htdocs/note-opencart/system/');
+define('DIR_LANGUAGE', 'C:/htdocs/note-opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:/htdocs/note-opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:/htdocs/note-opencart/system/config/');
+define('DIR_IMAGE', 'C:/htdocs/note-opencart/image/');
+define('DIR_CACHE', 'C:/htdocs/note-opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/htdocs/note-opencart/system/storage/download/');
+define('DIR_LOGS', 'C:/htdocs/note-opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/htdocs/note-opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/htdocs/note-opencart/system/storage/upload/');
+define('DIR_CATALOG', 'C:/htdocs/note-opencart/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://127.0.0.1/note-opencart/');
 define('HTTPS_SERVER', 'http://127.0.0.1/note-opencart/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/htdocs/note-opencart/catalog/');
-define('DIR_SYSTEM', 'D:/htdocs/note-opencart/system/');
-define('DIR_LANGUAGE', 'D:/htdocs/note-opencart/catalog/language/');
-define('DIR_TEMPLATE', 'D:/htdocs/note-opencart/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/htdocs/note-opencart/system/config/');
-define('DIR_IMAGE', 'D:/htdocs/note-opencart/image/');
-define('DIR_CACHE', 'D:/htdocs/note-opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/htdocs/note-opencart/system/storage/download/');
-define('DIR_LOGS', 'D:/htdocs/note-opencart/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/htdocs/note-opencart/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/htdocs/note-opencart/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/htdocs/note-opencart/catalog/');
+define('DIR_SYSTEM', 'C:/htdocs/note-opencart/system/');
+define('DIR_LANGUAGE', 'C:/htdocs/note-opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/htdocs/note-opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/htdocs/note-opencart/system/config/');
+define('DIR_IMAGE', 'C:/htdocs/note-opencart/image/');
+define('DIR_CACHE', 'C:/htdocs/note-opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/htdocs/note-opencart/system/storage/download/');
+define('DIR_LOGS', 'C:/htdocs/note-opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/htdocs/note-opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/htdocs/note-opencart/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
